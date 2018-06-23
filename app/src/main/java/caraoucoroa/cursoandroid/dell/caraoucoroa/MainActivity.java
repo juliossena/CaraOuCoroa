753cd8f2705f29d4d448a518fbc1242e892d0809
juliossena <juliosouzasena@gmail.com>
2018-06-23 19:21:50 -0300

    final

--- app/src/main/java/caraoucoroa/cursoandroid/dell/caraoucoroa/MainActivity.java
+++ app/src/main/java/caraoucoroa/cursoandroid/dell/caraoucoroa/MainActivity.java
@@ -1,13 +1,36 @@
 package caraoucoroa.cursoandroid.dell.caraoucoroa;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    private ImageView btnJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnJogar = (ImageView) findViewById(R.id.btn_jogar_id);
+
+        btnJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random r = new Random();
+
+                Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+                intent.putExtra("sorteio", r.nextInt(2));
+
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/caraoucoroa/cursoandroid/dell/caraoucoroa/SegundaActivity.java
@@ -0,0 +1,46 @@
+package caraoucoroa.cursoandroid.dell.caraoucoroa;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class SegundaActivity extends Activity {
+
+    private ImageView imagemResultado;
+    private ImageView btnVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        imagemResultado = (ImageView) findViewById(R.id.img_resultado_id);
+        btnVoltar = (ImageView) findViewById(R.id.btn_voltar_id);
+
+        Bundle b = getIntent().getExtras();
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SegundaActivity.this, MainActivity.class));
+            }
+        });
+
+        if (b != null) {
+            int valor = (int) b.get("sorteio");
+            Log.d("aqui", valor + "");
+            if (valor == 1)
+                imagemResultado.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_cara));
+            else {
+                imagemResultado.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_coroa));
+            }
+        }
+    }
+}
