221fe8a678a8fd1f1fc91687a7529553801daf63
juliossena <juliosouzasena@gmail.com>
2018-06-23 18:31:41 -0300

    inicio

---
+++ app/src/main/java/caraoucoroa/cursoandroid/dell/caraoucoroa/MainActivity.java
@@ -0,0 +1,13 @@
+package caraoucoroa.cursoandroid.dell.caraoucoroa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
